7bf5eb5c6a0c57e9fd999c7b54339de6bbc13faf
Brandon LeBeau <lebebr01@gmail.com>
2016-08-31 13:38:51 -0500

    New headings function.

--- R/directory_search.r
+++ R/directory_search.r
@@ -9,24 +9,35 @@
 #'    surrouding lines of text in addition to the matching line. Default is FALSE, if 
 #'    not false, include a numeric number that indicates the additional number of 
 #'    surrounding lines that will be extracted.
-#' @param ignore.case TRUE/FALSE indicating whether the case of the keyword matters. 
-#'    Default is FALSE meaning that case of the keyword is literal.
+#' @param ignore.case TRUE/FALSE/vector of TRUE/FALSE, indicating whether the case of the keyword matters. 
+#'    Default is FALSE meaning that case of the keyword is literal. If a vector, 
+#'    must be same length as the keyword vector.
 #' @param full_names TRUE/FALSE indicating if the full file path should be used.
 #'    Default is FALSE, see \code{\link{list.files}} for more details.
 #' @param recursive TRUE/FALSE indicating if subdirectories should be searched as well.
 #'    Default is FALSE, see \code{\link{list.files}} for more details.
+#' @param max_search An optional numeric vector indicating the maximum number
+#'    of pdfs to search. Will only search the first n cases.
 #' @export
 keyword_directory <- function(directory, keyword, surround_lines = FALSE,
-                              ignore.case = FALSE,
-                              full_names = FALSE, recursive = FALSE) {
+                              ignore.case = FALSE, full_names = FALSE, 
+                              recursive = FALSE, max_search = NULL) {
   files_dir <- list.files(path = directory, pattern = ".pdf", 
                           full.names = full_names, recursive = recursive)
   file_name <- list.files(path = directory, pattern = ".pdf", 
                           full.names = FALSE, recursive = recursive)
   
-  extract_table <- lapply(seq_along(files_dir), function(xx) 
-    keyword_search(files_dir[xx], keyword = keyword, path = TRUE,
-                   surround_lines = surround_lines, ignore.case = ignore.case))
+  if(is.null(max_search)) {
+    extract_table <- lapply(seq_along(files_dir), function(xx) 
+      keyword_search(files_dir[xx], keyword = keyword, path = TRUE,
+                     surround_lines = surround_lines, ignore.case = ignore.case))
+  } else {
+    files_dir <- files_dir[1:max_search]
+    file_name <- file_name[1:max_search]
+    extract_table <- lapply(seq_along(files_dir), function(xx) 
+      keyword_search(files_dir[xx], keyword = keyword, path = TRUE,
+                     surround_lines = surround_lines, ignore.case = ignore.case))
+  }
   
   num_rows <- unlist(lapply(extract_table, nrow))
   
---
+++ R/heading_search.r
@@ -0,0 +1,38 @@
+#' Function to locate sections of pdf
+#' 
+#' The ability to extract the location of the text and separate by sections. 
+#' The function will return the headings with their location in the pdf.
+#' 
+#' @param x Either the text of the pdf read in with the pdftools package or a path
+#'    for the location of the pdf file.
+#' @param headings A character vector representing the headings to search for.
+#'    Can be NULL if pdf_toc = TRUE.
+#' @param path An optional path designation for the location of the pdf to be converted 
+#'    to text. The pdftools package is used for this conversion.
+#' @param pdf_toc TRUE/FALSE whether the pdf_toc function should be used. This
+#'    is most useful if the pdf has the table of contents embedded within the pdf.
+#'    Must specify path = TRUE if pdf_toc = TRUE.
+#' @param full_line TRUE/FALSE indicating whether the headings should reside on
+#'    their own line. This can create problems with multiple column pdfs.
+#' @param ignore.case TRUE/FALSE/vector of TRUE/FALSE, indicating whether the case of the keyword matters. 
+#'    Default is FALSE meaning that case of the headings keywords are literal. 
+#'    If a vector, must be same length as the headings vector.
+#' @importFrom pdftools pdf_toc
+#' @export 
+heading_search <- function(x, headings, path = FALSE, pdf_toc = FALSE,
+                           full_line = FALSE, ignore.case = FALSE) {
+  
+  if(pdf_toc & !path) stop('If pdf_toc = TRUE, then path must be specified')
+  if(pdf_toc & path) {
+    toc <- pdftools::pdf_toc(x)
+    headings <- unlist(lapply(seq_along(toc$children[[1]]$children), function(xx) 
+      toc$children[[1]]$children[[xx]]$title))
+  }
+  if(full_line) {
+    headings <- paste0('^', headings, '$')
+  }
+  
+  keyword_search(x, keyword = headings, path = path, 
+                 surround_lines = FALSE, ignore.case = ignore.case)
+  
+}
