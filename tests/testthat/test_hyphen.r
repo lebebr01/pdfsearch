4172acdd9bc859406a41b8eeea37ea44f1e625a8
Brandon LeBeau <lebebr01@gmail.com>
2018-03-22 15:15:09 -0500

    Adjust keyword in test

--- tests/testthat/test_hyphen.r
+++ tests/testthat/test_hyphen.r
@@ -3,8 +3,8 @@ context('remove hyphens')
 test_that('remove hyphens', {
   path <- system.file('pdf', '1610.00147.pdf', package = 'pdfsearch')
   
-  hyphen_removed <- keyword_search(x = path, keyword = 'inferences', path = TRUE)
-  keep_hyphen <- keyword_search(x = path, keyword = 'inferences', path = TRUE,
+  hyphen_removed <- keyword_search(x = path, keyword = 'measurement', path = TRUE)
+  keep_hyphen <- keyword_search(x = path, keyword = 'measurement', path = TRUE,
                          remove_hyphen = FALSE)
   
   expect_false(isTRUE(all.equal(nrow(hyphen_removed), 
