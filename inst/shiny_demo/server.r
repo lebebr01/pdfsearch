fa2f044b8316b80258b5713b2915e4e467c663b9
Brandon LeBeau <lebebr01@gmail.com>
2018-05-24 13:22:13 -0500

    Add shiny demo of interactive selection

---
+++ inst/shiny_demo/server.r
@@ -0,0 +1,83 @@
+library(shiny)
+library(shinydashboard)
+library(pdfsearch)
+
+server <- function(input, output, session) {
+  
+  output$key_vals <- renderUI({
+    num_keys <- input$num_key
+    lapply(1:num_keys, function(i)
+      div(style = 'display:inline-block',
+          textInput(paste0('key', i), label = paste0('Keyword', i), 
+                    value = '', width = '75px'))
+    )
+  })
+  
+  output$vec_ignore <- renderUI({
+    num_keys <- input$num_key
+    lapply(1:num_keys, function(i)
+      div(style = 'display:inline-block',
+          radioButtons(paste0('ig_case', i), label = paste0('Ignore Case ', i), 
+                       choices = c('No' = FALSE, 'Yes' = TRUE),
+                       selected = 1))
+    )
+  })
+  
+  keyword_result <- eventReactive(input$run_search, {
+    num_files <- nrow(input$path)
+    num_keys <- input$num_key
+    keywords <- do.call('c', lapply(1:num_keys, function(xx) 
+      eval(parse(text = paste0('input$key', xx)))))
+    if(input$surround == 1) {
+      srd <- FALSE
+    } else {
+      srd <- input$num_surround
+    }
+    if(input$ignore_case == 3) {
+      ign_cs <- do.call('c', lapply(1:input$num_key, function(xx) 
+        eval(parse(text = paste0('ig_case', xx)))))
+    } else {
+      ign_cs <- input$ignore_case
+    }
+    do.call('rbind', lapply(1:num_files, function(xx) 
+      data.frame(ID = xx, keyword_search(input$path[[xx, 'datapath']], 
+                                         keyword = keywords,
+                                         path = TRUE,
+                                         surround_lines = srd,
+                                         ignore_case = ign_cs)))
+    )
+  })
+  
+  output$select_result <- renderUI({
+    options <- keyword_result()[['line_text']]
+    
+    checkboxGroupInput('selected_group', 'Select Most Relevant Option:',
+                       choices = options)
+  })
+  
+  output$search_results <- renderDataTable({
+    keyword_result()
+  })
+  
+  keyword_flatten <- reactive({
+    text_lines <- data.frame(do.call('rbind', keyword_result()[['line_text']]))
+    if(input$surround == 1) {
+      srd <- 0
+    } else {
+      srd <- input$num_surround
+    }
+    names(text_lines) <- paste0('line_text_', 1:ncol(text_lines))
+    key_flat <- keyword_result()[c('keyword', 'page_num', 'line_num')]
+    cbind(key_flat, text_lines)
+  })
+  
+  output$down_results <- downloadHandler(
+    filename = function() { 
+      paste0('results', '.csv')
+    },
+    content = function(file) {
+      write.csv(keyword_flatten(), file)
+    }
+  )
+  
+}
---
+++ inst/shiny_demo/ui.r
@@ -0,0 +1,83 @@
+library(shinydashboard)
+library(DT)
+
+ui <- dashboardPage(skin = "green",
+                    dashboardHeader(title = "pdfsearch Demo 2"),
+                    dashboardSidebar(
+                      sidebarMenu(
+                        menuItem('Search Options', tabName = 'search', 
+                                 icon = icon('info-circle')),
+                        menuItem('Results', tabName = 'results',
+                                 icon = icon('table'))
+                      )
+                    ),
+                    dashboardBody(
+                      tabItems(
+                        tabItem(tabName = 'search',
+                                h2('Enter pdf search parameters'),
+                                h3('Input basic pdf search parameters to include in pdf search'),
+                                fluidRow(
+                                  box(title = 'Select pdf file to search',
+                                      status = 'primary', width = 4,
+                                      fileInput('path', 'Choose pdf file(s)',
+                                                multiple = TRUE,
+                                                accept = '.pdf'),
+                                      p('You may select more than one pdf file'),
+                                      hr(),
+                                      radioButtons('surround', 'Extract surrounding lines?',
+                                                   choices = c('No' = 1, 'Yes' = 2),
+                                                   selected = 1),
+                                      conditionalPanel(
+                                        condition = 'input.surround == 2',
+                                        numericInput('num_surround', 'Number of Surrounding lines to extract',
+                                                     val = 1, min = 1, max = NA)
+                                      )
+                                  ),
+                                  box(title = 'Keywords',
+                                      status = 'primary', width = 6,
+                                      numericInput('num_key', 'Number of Keywords', 
+                                                   value = 1, min = 1, max = NA),
+                                      uiOutput('key_vals'),
+                                      hr(),
+                                      radioButtons('ignore_case', 'Ignore Case in Search?',
+                                                   choices = c('No' = FALSE, 'Yes' = TRUE, 'Vector' = 3),
+                                                   selected = 1),
+                                      conditionalPanel(
+                                        condition = 'input.ignore_case == 3',
+                                        uiOutput('vec_ignore')
+                                      )
+                                  )
+                                ),
+                                fluidRow(
+                                  box(title = 'Run Search', status = 'warning',
+                                      width = 12,
+                                      actionButton('run_search', 'Run Search on PDFs uploaded')
+                                  )
+                                ),
+                                fluidRow(
+                                  box(title = 'Select Search Results', status = 'success',
+                                      width = 12,
+                                      uiOutput('select_result')
+                                      )
+                                )
+                        ),
+                        tabItem(tabName = 'results',
+                                h2('View search results'),
+                                fluidRow(
+                                  box(title = 'Table of Search Results',
+                                      status = 'primary',
+                                      width = 12,
+                                      dataTableOutput('search_results')
+                                  )
+                                ),
+                                fluidRow(
+                                  box(title = 'Download Results',
+                                      status = 'info',
+                                      width = 2,
+                                      downloadButton('down_results', 'Download'))
+                                )
+                                
+                        )
+                      )
+                    )
+)
