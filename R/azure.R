f01e1c7c82a92657c6f6816b9ffaf8f9958da04e
Carsten Behring <carsten.behring@gmail.com>
2020-11-30 16:12:11 +0100

    fixed chunking and all

--- R/azure.R
+++ R/azure.R
@@ -1,6 +1,23 @@
+
+
+
+
+remove_ws <- function(x) {
+  sapply(x,
+         FUN = function(s) { trimws(gsub("\\s+", " ", s)) },
+         USE.NAMES  =F
+         )
+}
+
+
+
+
+
+
+
 #' @importFrom  tokenizers tokenize_sentences
 #' @importFrom stringi stri_length stri_flatten stri_join
-#' @importFrom httr progress status_code upload_file GET POST headers content_type add_headers content
+#' @importFrom httr progress status_code upload_file GET POST headers content_type add_headers content stop_for_status
 do_ocr_pdf <- function(path,params,md5) {
   upload <- upload_file(path)
 
@@ -10,7 +27,8 @@ do_ocr_pdf <- function(path,params,md5) {
                                         #                       httr::verbose(),
                          progress("up"),
                          body=upload)
-  #print(post_response)
+                                        #print(post_response)
+  stop_for_status(post_response,paste0("OCR: ",content(post_response,as = "text")))
   response_headers <- headers(post_response)
                                         #print(response_headers)
 
@@ -21,6 +39,7 @@ do_ocr_pdf <- function(path,params,md5) {
     results_response <-
       GET(results_location,
           add_headers(`Ocp-Apim-Subscription-Key` = params$api_token))
+    stop_for_status(results_response)
     if (content(results_response)$status =="succeeded") {
       break
     }
@@ -33,17 +52,22 @@ do_ocr_pdf <- function(path,params,md5) {
 
   ocr_result <- content(results_response)
 
+  num_results <- length(ocr_result$analyzeResult$readResults)
+  lines <- c()
+  counter <- 1
+  for (i in 1:num_results) {
 
-  num_lines = length(ocr_result$analyzeResult$readResults[[1]]$lines)
+    num_lines = length(ocr_result$analyzeResult$readResults[[i]]$lines)
+    for (j in 1:num_lines) {
 
-  lines = c()
-
-  for (i in 1:num_lines) {
-    lines[i]=ocr_result$analyzeResult$readResults[[1]]$lines[[i]]$text
+      lines[counter]=trimws(gsub("\\s+", " ", ocr_result$analyzeResult$readResults[[i]]$lines[[j]]$text))
+      counter <- counter +1
+    }
   }
+  output_dir_name <- paste0(dirname(path),"/outputs/")
+  dir.create(output_dir_name,showWarnings = F)
+  writeLines(lines,file(paste0(output_dir_name,basename(path),".ocr.txt")))
 
-  dir.create("outputs",showWarnings = F)
-  writeLines(lines,file(paste0("outputs/",basename(path),".")))
   return(lines)
 
 }
@@ -107,10 +131,18 @@ split_string_max_10000 <- function(s) {
   result[chunk_counter] <- current_string
   return(result)
 }
+chunk_list <- function(d,n) {
 
+ split(d, ceiling(seq_along(d)/n))
+}
 translate_chunk <- function(x,translation_target_language,params) {
   texts_to_translate <- lapply(x, function(s) {list(text=s)})
-  
+
+  print("translate chunk")
+  x <- remove_ws(x)
+  print(paste("#lines: ", length(x)));
+  body <- jsonlite::toJSON(texts_to_translate,auto_unbox = T)
+  print(paste("body size: " , stri_length(body) ))
  #browser()
   
   
@@ -120,30 +152,34 @@ translate_chunk <- function(x,translation_target_language,params) {
                  encode = "json",
                  content_type("application/json"),
                  #verbose(),
-                 body = jsonlite::toJSON(texts_to_translate,auto_unbox = T)
-  )
+                 body = body
+                 )
+  stop_for_status(result,paste0("tranlate: ",content(result,as="text")))
   print(result)
   if (status_code(result)!=200) {
     print(content(result))
     stop("non 200 status code")
     
   }
+
+
   translation <- sapply(content(result),function(res) {res$translations[[1]]$text})
+
+
   return(translation)
   
   
 }
-do_translate <- function(x,translation_target_language,params) {
+do_translate <- function(x,path,translation_target_language,params) {
 
-
- print("do translation")
+  print("do translation")
+  x <- remove_ws(x)
   print(paste("#lines: ", length(x)));
   text_length <- sum(stri_length(stri_flatten(x)))
   print(paste("text length: " ,text_length))
 
   
-  chunks <- tokenize_sentences( stri_flatten(x))
-  
+  chunks <- chunk_list(tokenize_sentences( stri_flatten(x))[[1]],30)
 
   results = list()
   for (chunk in chunks) {
@@ -161,14 +197,20 @@ do_translate <- function(x,translation_target_language,params) {
   #   warning("more then 100 texts , not tranlated")
   #   return(x)
   # }
-
+  results <- unlist(results)
+  output_dir_name <- paste0(dirname(path),"/outputs/")
+  dir.create(output_dir_name,showWarnings = F)
+  writeLines(results,file(paste0(output_dir_name,
+                               basename(path),".",
+                               translation_target_language, ".txt")))
+  return(results)
 }
 
 
 mem_translate <- memoise::memoise(do_translate,cache=fc)
 
-translate <- function(x,translation_target_language,params) {
-  mem_translate(x,translation_target_language,params)
+translate <- function(x,path,translation_target_language,params) {
+  mem_translate(x,path,translation_target_language,params)
 }
 
 
