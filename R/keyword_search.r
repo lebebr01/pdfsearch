54b3e2105b101c7ab40d3a0ff101472dc91b7668
Brandon LeBeau <lebebr01@gmail.com>
2022-04-01 09:46:18 -0500

    Update keyword_search to use new format function

--- R/keyword_search.r
+++ R/keyword_search.r
@@ -10,21 +10,14 @@
 #'    keywords can be specified with a character vector.
 #' @param path An optional path designation for the location of the pdf to be 
 #'    converted to text. The pdftools package is used for this conversion.
-#' @param split_pdf TRUE/FALSE indicating whether to split the pdf using white 
-#'    space. This would be most useful with multicolumn pdf files. 
-#'    The split_pdf function attempts to recreate the column layout of the text 
-#'    into a single column starting with the left column and proceeding to the 
-#'    right.
 #' @param surround_lines numeric/FALSE indicating whether the output should 
-#'    extract the surrouding lines of text in addition to the matching line. 
+#'    extract the surrounding lines of text in addition to the matching line. 
 #'    Default is FALSE, if not false, include a numeric number that indicates 
 #'    the additional number of surrounding lines that will be extracted.
 #' @param ignore_case TRUE/FALSE/vector of TRUE/FALSE, indicating whether the 
 #'    case of the keyword matters. Default is FALSE meaning that case of the 
 #'    keyword is literal. If a vector, must be same length as the keyword 
 #'    vector.
-#' @param remove_hyphen TRUE/FALSE indicating whether hyphenated words should
-#'    be adjusted to combine onto a single line. Default is TRUE.
 #' @param token_results TRUE/FALSE indicating whether the results text returned
 #'    should be split into tokens. See the tokenizers package and 
 #'    \code{\link{convert_tokens}} for more details. Defaults to TRUE.
@@ -33,6 +26,13 @@
 #' @param heading_args A list of arguments to pass on to the 
 #'    \code{\link{heading_search}} function. See \code{\link{heading_search}} 
 #'     for more details on arguments needed.
+#' @param split_pdf TRUE/FALSE indicating whether to split the pdf using white 
+#'    space. This would be most useful with multicolumn pdf files. 
+#'    The split_pdf function attempts to recreate the column layout of the text 
+#'    into a single column starting with the left column and proceeding to the 
+#'    right.
+#' @param remove_hyphen TRUE/FALSE indicating whether hyphenated words should
+#'    be adjusted to combine onto a single line. Default is TRUE.
 #' @param convert_sentence TRUE/FALSE indicating if individual lines of PDF file
 #'     should be collapsed into a single large paragraph to perform keyword 
 #'     searching. Default is TRUE
@@ -55,7 +55,6 @@
 #'   
 #' @importFrom pdftools pdf_text
 #' @importFrom tibble tibble
-#' @importFrom stringi stri_split_boundaries stri_split_lines
 #' @examples 
 #' file <- system.file('pdf', '1501.00450.pdf', package = 'pdfsearch')
 #' 
@@ -71,17 +70,18 @@
 #'   path = TRUE, split_pdf = TRUE, remove_hyphen = FALSE)
 #' 
 #' @export
-keyword_search <- function(x, keyword, path = FALSE, split_pdf = FALSE,
+keyword_search <- function(x, keyword, path = FALSE, 
                            surround_lines = FALSE, ignore_case = FALSE,
-                           remove_hyphen = TRUE, token_results = TRUE,
+                           token_results = TRUE,
                            heading_search = FALSE, heading_args = NULL,
+                           split_pdf = FALSE, remove_hyphen = TRUE,
                            convert_sentence = TRUE, 
                            remove_equations = FALSE,
                            split_pattern = "\\p{WHITE_SPACE}{3,}", ...) {
   if(path) {
-    x <- pdftools::pdf_text(x)
+    pdf_text <- pdftools::pdf_text(x)
   }
-  line_nums <- cumsum(lapply(tokenizers::tokenize_lines(x), length))
+  line_nums <- cumsum(lapply(tokenizers::tokenize_lines(pdf_text), length))
   if(any(line_nums == 0)) {
     warning('text not recognized in pdf')
     text_out <- data.frame(keyword = NULL, 
@@ -90,33 +90,9 @@ keyword_search <- function(x, keyword, path = FALSE, split_pdf = FALSE,
                                line_text = NULL)
   } else {
     
-    if(split_pdf) {
-      x_list <- split_pdf(x, pattern = split_pattern)
-      x_lines_list <- x_list
-    } else {
-      x_lines_list <- stringi::stri_split_lines(x)
-    }
-    
-    x_lines_list <- lapply(seq_along(x_lines_list), function(xx) gsub("^\\s+|\\s+$", '', 
-                                                 x = x_lines_list[[xx]]))
-    
-    if(remove_hyphen) {
-      x_lines_list <- lapply(x_lines_list, remove_hyphen)
-    }
-    
-    if(remove_equations) {
-      x_lines_list <- lapply(x_lines_list, remove_equation)
-    }
-    
-    # collapse into a single paragraph
-    if(convert_sentence) {
-      x_lines_list <- lapply(seq_along(x_lines_list), function(xx) paste(x_lines_list[[xx]], collapse = ' ')
-      )
-      x_lines_list <- lapply(seq_along(x_lines_list), function(xx) unlist(stringi::stri_split_boundaries(x_lines_list[[xx]], 
-                                                       type = "sentence"))
-                             )
-      line_nums <- cumsum(unlist(lapply(x_lines_list, length)))
-    }
+    x_lines_list <- format_text(pdf_text, split_pdf, 
+                                remove_hyphen, convert_sentence,
+                                remove_equations, split_pattern)
     
     x_lines <- unlist(x_lines_list)
     
